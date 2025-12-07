3fc9ad160faa19609f3d2fddcd2d0cb1220cd3b2
Rakibul29h <rakibul2170@gmail.com>
2025-12-07 19:35:12 +0600

    User save in Database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
  require('dotenv').config()
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 3000
 
 const app = express()
+
+const uri=process.env.MONGODB_KEY
 // middleware
 app.use(
   cors({
@@ -19,9 +22,34 @@ app.use(
 app.use(express.json())
 
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
+async function run() {
+  try {
+    const db=client.db("LocalChefBazaar")
+    const usersCollection=db.collection("Users");
 
+    app.post("/user",async(req,res)=>{
 
+      const userData=req.body;
+      userData.role="Customer";
+      const result = await usersCollection.insertOne(userData);
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 
