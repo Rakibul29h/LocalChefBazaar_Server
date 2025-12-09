05f79adf64c523e5aad0ddb187569b415fa5516f
Rakibul29h <rakibul2170@gmail.com>
2025-12-09 10:25:54 +0600

    JWT Authoriztion verify done

--- index.js
+++ index.js
@@ -1,62 +1,99 @@
- require('dotenv').config()
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
-const port = process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-const uri=process.env.MONGODB_KEY
+const uri = process.env.MONGODB_KEY;
 // middleware
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
     optionSuccessStatus: 200,
   })
-)
-app.use(express.json())
-
+);
+app.use(express.json());
+app.use(cookieParser());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
+// JWT Token
+
+const verifyJWT = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  // verify jwt
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.token_email = decoded.email;
+  });
+  next();
+};
 async function run() {
   try {
-    const db=client.db("LocalChefBazaar")
-    const usersCollection=db.collection("Users");
+    const db = client.db("LocalChefBazaar");
+    const usersCollection = db.collection("Users");
 
-    app.post("/user",async(req,res)=>{
+    // create JWT TOKEN :
 
-      const userData=req.body;
-      userData.role="Customer";
+    app.post("/getToken", (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1d" });
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      res.send({ success: true });
+    });
+
+    // Clear Cookies when User LogOut
+    app.post("/logout", (req, res) => {
+      res.clearCookie("token", {
+        httpOnly: true,
+      });
+      res.send({ success: true });
+    });
+
+    app.post("/user", async (req, res) => {
+      const userData = req.body;
+      userData.role = "Customer";
       const result = await usersCollection.insertOne(userData);
       res.send(result);
-    })
+    });
 
+    app.get("/xyz", verifyJWT, (req, res) => {
+      const tokenEmail = req.token_email;
+      res.send({ message: "done", email: tokenEmail });
+    });
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello from Server..')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Server..");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
